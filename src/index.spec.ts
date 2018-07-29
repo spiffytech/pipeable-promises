8591003af3306f8427feb20ee1fc78af8717a075
Brian Cottingham <spiffytech@gmail.com>
2018-07-29 09:52:04 -0400

    Backed out Sanctuary support. Typing for it was a pain, and it's actually easier and better to let users use Sanctuary functions as their then/catch handlers.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,8 @@
 import {flow} from 'lodash/fp';
-import S from 'sanctuary';
 
 import * as lib from './index';
 const then = lib.then;
-const thenChain = lib.thenChain;
+const catchP = lib.catchP;
 
 describe('It chains promises', () => {
   it('chains simple promises', () => {
@@ -21,41 +20,13 @@ describe('It chains promises', () => {
       then((n) => expect(n).toEqual(21)),
     )(42);
   });
-});
 
-describe('It chains Sanctuary types', () => {
-  it('chains Rights', () => {
+  it('catches promises', () => {
     return flow(
-      (n: number) => Promise.resolve(S.Right(n)),
-      thenChain((n) => n * 2),
-      thenChain((n) => expect(n).toEqual(84)),
-    )(42);
-  });
-
-  it('chains Maybes', () => {
-    return flow(
-      (n: number) => Promise.resolve(S.Just(n)),
-      thenChain((n) => n * 2),
-      thenChain((n) => expect(n).toEqual(84)),
-    )(42);
-  });
-
-  it('doesn\'t chain Lefts', () => {
-    return flow(
-      (n: number) => Promise.resolve(S.Left(n)),
-      thenChain((n) => n * 2),
-      then((v) => {
-        expect(S.isLeft(v)).toBe(true)
-        S.bimap((l) => expect(l).toEqual(42));
-      }),
-    )(42);
-  });
-
-  it('doesn\'t chain Nothing', () => {
-    return flow(
-      (n: number) => Promise.resolve(S.Nothing),
-      thenChain((n) => n * 2),
-      then((v) => expect(S.isNothing(v)).toBe(true)),
+      (n: number): Promise<number> => Promise.resolve(n),
+      then((n) => Promise.reject(n / 2)),
+      catchP((n) => n),
+      then((n) => expect(n).toEqual(21)),
     )(42);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,7 @@
-import S from 'sanctuary';
-
-interface Functor<A> {
-  constructor: {
-    '@@type': 'sanctuary/Functor';
-  };
-}
-
-interface Either<A, B> {
-  constructor: {
-    '@@type': 'sanctuary/Either';
-  };
-}
-
-interface Maybe<A> {
-  constructor: {
-    '@@type': 'sanctuary/Maybe';
-  };
-}
-
-function isEither<T, U>(val: Either<T, U> | any): val is Either<T, U> {
-  return S.type(val).name === 'Either';
-}
-
-function isMaybe<T>(val: Maybe<T> | any): val is Maybe<T> {
-  return S.type(val).name === 'Maybe';
-}
-
 export function then<T, U>(fn: (arg: T) => U) {
   return function thenPromise(p: Promise<T>): Promise<U> {
     return p.then(fn);
   };
-}
-
-export function thenChain<T, U>(fn: (arg: T) => U) {
-  function thenPromiseChain<V>(p: Promise<Either<T, V>>): Promise<Either<T, U>>;
-  function thenPromiseChain(p: Promise<Maybe<T>>): Promise<Maybe<U>>;
-  function thenPromiseChain(p: Promise<T>): Promise<U>;
-  function thenPromiseChain<V>(p: Promise<Either<V, T> | Maybe<T> | T>): Promise<Either<V, U> | Maybe<U> | U> {
-    return p.then((val) => {
-      if (isEither(val)) return S.map(fn)(val);
-      if (isMaybe(val)) return S.map(fn)(val);
-      return fn(val);
-    });
-  }
-
-  return thenPromiseChain;
 }
 
 export function catchP<T, U, V>(fn: (arg: U) => V) {
